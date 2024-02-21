ae0bccff03a19a0d891fe08c483d37f771786e7c
Kamil Najdek <259011@student.pwr.edu.pl>
2024-02-21 13:33:58 +0100

    change window size and close it

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,3 +1,6 @@
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -8,7 +11,13 @@ public class SeleniumTest {
     @Test
     public void openGooglePage() {
         WebDriver driver = getDriver("chrome");
+        driver.manage().window().maximize();
         driver.get("https://www.google.com");
+        Dimension windowSize = new Dimension(1000, 1000);
+        driver.manage().window().setSize(windowSize);
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("window.open('https://www.google.com','blank','height=500,width=500')");
+        driver.close();
     }
 
     public WebDriver getDriver(String browser) {
